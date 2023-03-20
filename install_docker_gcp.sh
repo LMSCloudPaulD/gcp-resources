c4a06f1ffc8315364a870f0e830cf16484469a3d
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-03-20 12:48:18 +0100

    Adjust repository url and migrate to new approach of managing keys

--- install_docker_gcp.sh
+++ install_docker_gcp.sh
@@ -13,17 +13,15 @@ apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common \
+    gnupg2
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-
 apt-get install -y docker-ce
 
 groupadd docker
