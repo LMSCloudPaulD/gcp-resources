1d5ee009499fdcfb97733b836b1c00972426f1c6
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-03-20 13:13:21 +0100

    Add check for rsync and install if not present

--- setup-image-build.sh
+++ setup-image-build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Check if running with root privileges
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root. Please enter your sudo password below:"
+    # Prompt for sudo password
+    exec sudo "$0" "$@"
+fi
+
 # Check if Docker is installed
 if ! [ -x "$(command -v docker)" ]; then
-    echo "Error: Docker is not installed. Please run the previous script to install Docker first." >&2
+    echo "Error: Docker is not installed. Please install Docker first." >&2
     exit 1
 fi
 
 # Check if user is part of docker group
 if ! groups | grep "\bdocker\b" >/dev/null; then
-    echo "Error: User is not part of the docker group. Please run the previous script to add the user to the docker group." >&2
+    echo "Error: User is not part of the docker group. Please add the user to the docker group." >&2
     exit 1
 fi
 
@@ -24,6 +31,13 @@ if [ ! -d "koha-testing-docker" ]; then
     exit 1
 fi
 
+# Install rsync if not present
+if ! [ -x "$(command -v rsync)" ]; then
+    echo "rsync not found. Installing rsync..." >&2
+    apt-get update
+    apt-get install -y rsync
+fi
+
 # Set up required environment variables
 export LMS_PROJECTS_DIR=~/.local/src/lmsc
 export LMS_PROJECTS_DIR="$LMS_PROJECTS_DIR"
@@ -32,7 +46,7 @@ export LMS_KTD_HOME=$LMS_PROJECTS_DIR/LMSTestingDocker
 
 # Check if required environment variables are set up
 if [ -z "$LMS_PROJECTS_DIR" ] || [ -z "$LMS_SYNC_REPO" ] || [ -z "$LMS_KTD_HOME" ]; then
-    echo "Required environment variables not found. Please set up the environment variables first." >&2
+    echo "Error: Required environment variables not found. Please set up the environment variables first." >&2
     exit 1
 fi
 
@@ -40,6 +54,5 @@ fi
 git clone https://github.com/LMSCloud/LMSTestingDocker.git $LMS_KTD_HOME
 
 cd $KTD_HOME
-git checkout -b ktd-lms
-rsync -a --exclude='*.md' $LMS_KTD_HOME/* $KTD_HOME
+git checkout -b k
 
